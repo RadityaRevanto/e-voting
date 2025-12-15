4109c5574b66c224c50157c097040ada50188055
AdtyEka <eka255765@gmail.com>
2025-12-16 02:17:37 +0700

    Enhance Hero Section: Redesigned the Hero component with a new layout, added engaging content, and integrated visual elements to improve user experience on the welcome page.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/layouts/navbar";
+import Hero from "@/components/sections/hero";
 
 export default function Home() {
     return (
         <div className="w-full min-h-screen overflow-x-hidden relative">
             <Navbar />
+            <Hero />
         </div>
     )
 }
