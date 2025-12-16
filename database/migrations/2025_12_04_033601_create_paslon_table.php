9e68352a56ace8cc6d30372c32fb0b3605df38d1
Moch Raditya Revanto <mochradityarevantoo@gmail.com>
2025-12-17 03:35:05 +0700

    feat: [RD] Menambahkan Visi dan Misi

--- database/migrations/2025_12_04_033601_create_paslon_table.php
+++ database/migrations/2025_12_04_033601_create_paslon_table.php
@@ -9,25 +9,29 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('paslon', function (Blueprint $table) {
-            $table->id();
-            $table->integer('user_id');
-            $table->string('nama_ketua', length:100);
-            $table->string('jurusan_ketua', length:50)->nullable();
-            $table->string('nama_wakil_ketua', length:100);
-            $table->string('jurusan_wakil_ketua', length:50)->nullable();
-            $table->text('visi')->nullable();
-            $table->text('misi')->nullable();
-            $table->timestamps();
+// database/migrations/2025_12_04_033601_create_paslon_table.php
+public function up(): void
+{
+    Schema::create('paslon', function (Blueprint $table) {
+        $table->id();
+        $table->integer('user_id');
+        $table->string('nama_ketua', length:100);
+        $table->integer('umur_ketua')->nullable();
+        $table->string('jurusan_ketua', length:50)->nullable();
+        $table->string('nama_wakil_ketua', length:100);
+        $table->integer('umur_wakil_ketua')->nullable();
+        $table->string('jurusan_wakil_ketua', length:50)->nullable();
+        $table->string('foto_paslon')->nullable();
+        $table->text('visi')->nullable();
+        $table->text('misi')->nullable();
+        $table->timestamps();
 
-            $table->foreign('user_id')
-            ->references('id')
-            ->on('users')
-            ->onDelete('cascade');
-        });
-    }
+        $table->foreign('user_id')
+        ->references('id')
+        ->on('users')
+        ->onDelete('cascade');
+    });
+}
 
     /**
      * Reverse the migrations.
