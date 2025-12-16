a2eef50cceae2ad4a6ee85245ca136fdf5f15544
AdtyEka <eka255765@gmail.com>
2025-12-16 13:43:14 +0700

    Add Parallax Animation and Integrate Identity Component: Introduced parallax animations for left and right transitions in CSS, and added Identity component to the welcome page for enhanced visual appeal and content presentation.

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/layouts/navbar";
 import Hero from "@/components/sections/hero";
+import Identity from "@/components/sections/identy";
 
 export default function Home() {
     return (
         <div className="w-full min-h-screen overflow-x-hidden relative">
             <Navbar />
             <Hero />
+            <Identity />
         </div>
     )
 }
