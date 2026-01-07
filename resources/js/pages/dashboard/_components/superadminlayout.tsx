99866099a1b34417053bed79c34062013646d8b1
AdtyEka <eka255765@gmail.com>
2026-01-08 00:17:22 +0700

    nambah  overflowhidden

--- resources/js/pages/dashboard/_components/superadminlayout.tsx
+++ resources/js/pages/dashboard/_components/superadminlayout.tsx
@@ -123,9 +123,9 @@ export default function SuperadminLayout({ children }: SuperadminLayoutProps) {
   useRoleSwitch();
   
   return (
-    <SidebarProvider>
+    <SidebarProvider className="overflow-hidden">
       <AppSidebarSuperadmin />
-      <SidebarInset>
+      <SidebarInset className="overflow-hidden">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
